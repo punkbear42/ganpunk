1af09f5a9a9d7fdfbc8e82f575897b09467aba0e
punkbear42 <punkbear@protonmail.com>
2021-12-27 17:21:29 +0100

    react app

--- model-js/convert_to_js.py
+++ model-js/convert_to_js.py
@@ -4,5 +4,5 @@ from tensorflow import keras
 print(tensorflowjs.__version__)
 
 # convert keras model to tensorflow js
-model = keras.models.load_model('./results/generator_model_e_158_b_1500.h5')
+model = keras.models.load_model('./model-js/dropout_06_110.h5')
 tensorflowjs.converters.save_keras_model(model, './')
